c345c264061ca4b82676a94b823a4ecf9b2fb586
Bryan Chan <cky.bryan@gmail.com>
2020-01-14 11:23:33 -0500

    ch2090/move-btn

---
+++ src/styles/colors.ts
@@ -0,0 +1,19 @@
+interface Colors {
+  [index: string]: string;
+}
+
+const colors: Colors = {
+  APPLE_BLUE: "#007AFF",
+  BLACK: "#000000",
+  BLUE: "#4a90e2",
+  CHARCOAL: "#404041",
+  GREEN: "#37bc9b",
+  GREY: "#9b9b9b",
+  GREY_LIGHT: "#eeeeee",
+  GREY_MID: "#d5d5d5",
+  RED: "#da4453",
+  YELLOW: "#ffd33d",
+  WHITE: "#ffffff",
+};
+
+export default colors;
---
+++ src/styles/container.ts
@@ -0,0 +1,5 @@
+export default {
+  PADDING: {
+    HORIZONTAL: 20,
+  },
+};
