b5a701f1e495b17d2b501148e8a6854b75000812
Anthony Langford <anthony@secondcloset.com>
2020-01-22 15:50:37 -0500

    OPREF/colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,4 +1,19 @@
 interface Colors {
+  APPLE_BLUE: string;
+  BLACK: string;
+  BLUE: string;
+  CHARCOAL: string;
+  ORANGE: string;
+  GREEN: string;
+  LIME_GREEN: string;
+  GREY: string;
+  GREY_LIGHT: string;
+  GREY_MID: string;
+  RED: string;
+  YELLOW: string;
+  WHITE: string;
+  TRANSPARENT: string;
+  TRANSPARENT_GREY: string;
   [index: string]: string;
 }
 
@@ -9,7 +24,8 @@ const colors: Colors = {
   CHARCOAL: "#404041",
   ORANGE: "#F5A623",
   GREEN: "#37BC9B",
-  GREY: "#9B9B9B",
+  LIME_GREEN: "#49DB86",
+  GREY: "#9b9b9b",
   GREY_LIGHT: "#eeeeee",
   GREY_MID: "#d5d5d5",
   RED: "#FF3A52",
