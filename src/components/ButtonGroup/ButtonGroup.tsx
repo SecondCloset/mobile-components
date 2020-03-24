d790f14a67d659b8292cc0cb0d833585ab2a5884
Bryan Chan <cky.bryan@gmail.com>
2020-03-24 13:13:54 -0400

    ch3609/update-button-group

--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -1,46 +1,59 @@
-import React, { useState } from "react";
+import React from "react";
 import { Container, Button, ButtonText, TitleText } from "./styles";
 
 interface Option {
   text: string;
   subtext?: string;
+  disabled?: boolean;
 }
 
 interface ButtonGroupProps {
   options: Option[];
-  defaultValue?: string;
-  onOptionSelect: (value: string) => any;
+  selectedOptionText?: Option["text"];
+  onOptionSelect: (optionText: Option["text"]) => any;
 }
 
 const ButtonGroup: React.FC<ButtonGroupProps> = ({
   options,
-  defaultValue,
+  selectedOptionText,
   onOptionSelect,
 }) => {
-  const [selectedValue, setSelectedValue] = useState(defaultValue);
-
-  const renderOption = (option: Option, isFirst?: boolean) => {
-    const { text, subtext } = option;
-    const selected = selectedValue === text;
+  const renderOption = (
+    option: Option,
+    isFirst: boolean = false,
+    isLast: boolean = false
+  ) => {
+    const { text, subtext, disabled = false } = option;
+    const selected = selectedOptionText === text;
 
     return (
       <Button
         key={text}
         isFirst={isFirst}
-        onPress={() => {
-          onOptionSelect(text);
-          setSelectedValue(text);
-        }}
+        isLast={isLast}
+        onPress={() => onOptionSelect(text)}
         selected={selected}
+        disabled={disabled}
+        isTwoButtons={options.length === 2}
       >
-        <TitleText selected={selected}>{text}</TitleText>
-        {subtext && <ButtonText selected={selected}>{subtext}</ButtonText>}
+        <TitleText selected={selected} disabled={disabled}>
+          {text}
+        </TitleText>
+        {subtext && (
+          <ButtonText selected={selected} disabled={disabled}>
+            {subtext}
+          </ButtonText>
+        )}
       </Button>
     );
   };
 
   const renderOptions = () => {
-    return options.map((o, i) => renderOption(o, i === 0));
+    return options.map((o, i) => {
+      const isFirst = i === 0;
+      const isLast = i === options.length - 1;
+      return renderOption(o, isFirst, isLast);
+    });
   };
 
   return <Container>{renderOptions()}</Container>;
